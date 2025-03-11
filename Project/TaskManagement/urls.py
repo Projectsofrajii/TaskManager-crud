95d94f5689bf230d3eed145748668e5b5cdb8d2a
Projectsofrajii <Projectsofrajii>
2025-03-11 12:14:37 +0530

    modifictions

--- Project/TaskManagement/urls.py
+++ Project/TaskManagement/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     
     path('register/', RegisterAPIView.as_view(), name='register'), # register user, 
     path("login/", TokenObtainPairView.as_view(), name="login"), # login user, 
-    path('crud/', TaskAPIView.as_view(), name='crud'), # create task, 
-    path('crud/<str:title_id>/', TaskAPIView.as_view(), name='crud_param'), # update task, 
-    path('latestTask/', LatestTasksAPIView.as_view(), name='latestTask'), # delete task, 
+    path('crud/', TaskAPIView.as_view(), name='crud'), # task- create,readall , 
+    path('crud/<str:title_id>/', TaskAPIView.as_view(), name='crud_param'), # taskparam- get,update,delete 
+    path('latestTask/', LatestTasksAPIView.as_view(), name='latestTask'), #TaskList
+    path('TaskListlimit10/', TaskListlimit10.as_view(), name='TaskListlimit10'), # TaskListlimit10
 
 ]
--- Project/TaskManagement/views.py
+++ Project/TaskManagement/views.py
@@ -117,12 +117,12 @@ class LatestTasksAPIView(APIView):
     @handle_exceptions
     def get(self, request):
         paginators = self.pagination_class ()
-        tasks = Task.objects.only("title", "created_at").order_by('-created_at')[:10]  
+        tasks = Task.objects.only("title", "created_at").order_by('-created_at')  
         pages = paginators.paginate_queryset(tasks,request, view=self) 
         serializer = TaskSerializer(pages,many=True)
         return paginators.get_paginated_response(serializer.data)
     
-class LatestTasksAPIViewss(APIView): # this is problematic question so applied limit 10
+class TaskListlimit10(APIView): # this is problematic question so applied limit 10
     permission_classes = [IsAuthenticated]
     pagination_class = TaskPagination  # Apply pagination
 
